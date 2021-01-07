aa32bf3656dbd2b3bfd2751e3195150ad1168ed5
chahid2304 <76661169+chahid2304@users.noreply.github.com>
2021-01-07 15:37:17 +0100

    image_user

--- menu.php
+++ menu.php
@@ -73,12 +73,12 @@ session_start();
               <a class="dropdown-item" href="Contact.php">Contactez nous</a>
             </div>
           </li>
-
+           <li><a href="#"><img class="img-circle profile_img" src="img\pic.png" href="#" height=50 width=50 style="margin-right:20px;"></a></li>
         </ul>
-
+       
         <form class="form-inline mt-2 mt-md-0">
           <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit" style="margin-bottom:20px">Search</button>
         </form>
 
       </div>
