857c5b689009c653fbeef124249eed47c59d4f4c
reda-coder <redaelouahabi98@gmail.com>
2021-01-07 16:02:31 +0100

    btn blue

--- payement.php
+++ payement.php
@@ -29,8 +29,8 @@ if (isset($_GET['prix'])) {
    <style type="text/css">
 .payment{width: 500px; min-height: 200px; text-align: center; border: 1px solid #ddd; margin: 0 auto; padding:50px;}
 .payment h2{border-bottom: 1px solid #ddd; margin-bottom: 40px; padding-bottom: 10px;}
-.payment a{background: blue; border-radius: 3px; color:#fff; font-size:22px; padding:5px 30px;}
-.back a{width:150px; margin: 5px auto 0; padding: 7px 0; text-align: center; display: block; background: blue; border:1px solid #333; color: #fff; border-radius: 3px; font-size: 25px; }
+.payment a{background: #4883ff; border-radius: 3px; color:#fff; font-size:22px; padding:5px 30px;}
+.back a{width:150px; margin: 5px auto 0; padding: 7px 0; text-align: center; display: block; background: #4883ff; border:1px solid #333; color: #fff; border-radius: 3px; font-size: 25px; }
 </style>
 
 <div class="main">
