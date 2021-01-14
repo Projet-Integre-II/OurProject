f861f4f1278847c005caa1da8ff95f65da93c8a8
reda-coder <redaelouahabi98@gmail.com>
2021-01-14 12:24:44 +0100

    navbar fixed on  the top while scrolling down

--- menu.php
+++ menu.php
@@ -246,3 +246,19 @@ session_start();
         </div>
       </nav>
        -->
+       <script>
+       ///////////////// fixed menu on scroll for desktop
+if ($(window).width() > 992) {
+  $(window).scroll(function(){  
+     if ($(this).scrollTop() > 40) {
+        $('#navbar').addClass("fixed-top");
+        // add padding top to show content behind navbar
+        $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
+      }else{
+        $('#navbar').removeClass("fixed-top");
+         // remove padding top from body
+        $('body').css('padding-top', '0');
+      }   
+  });
+} // end if
+       </script>
